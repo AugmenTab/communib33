8dcc38d4ec8c005cc71690a5b7df3afb38ccdba1
AugmenTab <tbaum92@live.com>
2021-08-29 16:51:08 -0600

    kudob33 responds to commands.

--- bot/main.py
+++ bot/main.py
@@ -1,3 +1,30 @@
 #! python3
 
-print("Up and running!")
+# 3p Imports
+import discord
+
+# Internal Imports
+try:
+    from src.config import token
+except Exception as e:
+    print (e)
+
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print(f"We have logged in as {client.user}.")
+
+
+@client.event
+async def on_message(msg):
+    trigger = "beep"
+    response = "boop"
+    if trigger in str(msg.content):
+        await msg.channel.send(response)
+        return
+
+
+client.run(token)
