9583d4a16559092b8d73bde6ff6087fbce37ffe2
AugmenTab <tbaum92@live.com>
2021-08-30 12:07:43 -0600

    Kudos command set up, needs database.

--- bot/main.py
+++ bot/main.py
@@ -2,9 +2,11 @@
 
 # SL Imports
 import logging
+from typing_extensions import ParamSpecArgs
 
 # 3p Imports
 import discord
+from   discord.ext import commands
 
 # Internal Imports
 try:
@@ -13,15 +15,39 @@ except Exception as e:
     print (e)
 
 
-client = discord.Client()
+b33 = commands.Bot(command_prefix="!")
 
 
-@client.event
+@b33.event
 async def on_message(msg):
+    if msg.author == b33.user:
+        await b33.process_commands(msg)
+        return
     text = str(msg.content)
-    logging.warning(text)
     if "whats the website" in text.lower().replace("'", ""):
-        return await msg.reply("Here you go! https://www.niftyisland.com/")
+        await msg.reply("Here you go! https://www.niftyisland.com/")
+    await b33.process_commands(msg)
+
+
+@b33.command(name="kudos", help="Gives a user kudos, or checks your own kudos.")
+async def kudos(ctx):
+    giver = ctx.author.id
+    if len(ctx.message.mentions) > 0:
+        receiver = ctx.message.mentions[0]
+    else:
+        receiver = None
+    if not receiver:
+        await ctx.reply("Whoopsie! You've gotta @mention someone to give them kudos!")
+    elif receiver.bot:
+        await ctx.reply("Whoopsie! Bots like us have no use for kudos!")
+    elif giver == receiver.id:
+        ## TODO DM user with their own kudos count.
+        await ctx.send("Here are your kudos!")
+    else:
+        ## TODO Add a kudos to receiver.
+        logging.warning(receiver.id)
+        await ctx.message.add_reaction("⭐")
+
 
 if __name__ == "__main__":
-    client.run(token)
+    b33.run(token)
